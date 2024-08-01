8187ba9d07f1a7bd9576cd2add552aaa0aea4985
priyankanitinkatode <priyankamisal2512@gmail.com>
2024-08-01 10:38:34 +0000

    variables code done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=1345
+let  address="abc"
+var passward=123654
+city="wardha"
+emailId="abc@gmail.com"
+
+//accountId=2222 //not allowed
+
+/* 
+prefer not to used var
+bcz of issue in block and functional statement
+*/
+
+console.table([accountId,address,passward,city,emailId]);
